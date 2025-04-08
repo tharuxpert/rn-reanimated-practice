fc1bb469cf161c3427da1b81d69b5230a90d2511
tharuxpert <tharuxpert@gmail.com>
2025-04-08 10:39:40 +0530

    interpolate Colors

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
-import { useEffect } from "react";
-import { Button, Pressable, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  Button,
+  Dimensions,
+  Pressable,
+  StyleSheet,
+  Switch,
+  Text,
+  View,
+} from "react-native";
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
@@ -9,6 +17,8 @@ import Animated, {
   SharedValue,
   useAnimatedGestureHandler,
   useAnimatedScrollHandler,
+  interpolateColor,
+  useDerivedValue,
 } from "react-native-reanimated";
 import {
   GestureHandlerRootView,
@@ -17,15 +27,35 @@ import {
 } from "react-native-gesture-handler";
 import { Page } from "components/Page";
 
-const SIZE = 100.0;
+// const SIZE = 100.0;
 
-const CIRCLE_RADIUS = SIZE * 2;
+// const CIRCLE_RADIUS = SIZE * 2;
 
 const WORDS = ["What's", "up", "mobile", "devs?"];
 
 type ContextType = {
   translateX: number;
   translateY: number;
+};
+
+type Theme = "light" | "dark";
+
+const Colors = {
+  dark: {
+    background: "#1E1E1E",
+    circle: "#252525",
+    text: "#F8F8F8",
+  },
+  light: {
+    background: "#F8F8F8",
+    circle: "#FFF",
+    text: "#1E1E1E",
+  },
+};
+
+const SWITCH_TRACK_COLOR = {
+  true: "rgba(256,0,256,0.2)",
+  false: "rgba(0,0,0,0.1)",
 };
 
 export default function App() {
@@ -93,12 +123,57 @@ export default function App() {
   //   };
   // });
 
-  // ----------------------------Interpolate----------------------------
+  // ----------------------------Interpolate ScrollView----------------------------
 
-  const translateX = useSharedValue(0);
+  // const translateX = useSharedValue(0);
 
-  const scrollHandler = useAnimatedScrollHandler((event) => {
-    translateX.value = event.contentOffset.x;
+  // const scrollHandler = useAnimatedScrollHandler((event) => {
+  //   translateX.value = event.contentOffset.x;
+  // });
+
+  // ----------------------------Interpolate Colors----------------------------
+
+  const [theme, setTheme] = useState<Theme>("light");
+
+  // const progress = useSharedValue(0);
+  const progress = useDerivedValue(() => {
+    return theme === "dark" ? withTiming(1) : withTiming(0);
+  }, [theme]);
+
+  const rStyles = useAnimatedStyle(() => {
+    const backgroundColor = interpolateColor(
+      progress.value,
+      [0, 1],
+      [Colors.light.background, Colors.dark.background]
+    );
+
+    return {
+      backgroundColor,
+    };
+  });
+
+  const rCircleStyles = useAnimatedStyle(() => {
+    const backgroundColor = interpolateColor(
+      progress.value,
+      [0, 1],
+      [Colors.light.circle, Colors.dark.circle]
+    );
+
+    return {
+      backgroundColor,
+    };
+  });
+
+  const rTextStyles = useAnimatedStyle(() => {
+    const color = interpolateColor(
+      progress.value,
+      [0, 1],
+      [Colors.light.text, Colors.dark.text]
+    );
+
+    return {
+      color,
+    };
   });
 
   return (
@@ -116,31 +191,51 @@ export default function App() {
     //   </Pressable> */}
     // </GestureHandlerRootView>
 
-    // ----------------------------Interpolate----------------------------
+    // ----------------------------Interpolate ScrollView----------------------------
 
-    <Animated.ScrollView
-      pagingEnabled
-      horizontal
-      style={styles.container}
-      contentContainerStyle={styles.contentContainer}
-      onScroll={scrollHandler}
-      scrollEventThrottle={16}
-    >
-      {WORDS.map((title, index) => {
-        return <Page key={index.toString()} title={title} index={index} translateX={translateX} />;
-      })}
-    </Animated.ScrollView>
+    // <Animated.ScrollView
+    //   pagingEnabled
+    //   horizontal
+    //   style={styles.container}
+    //   contentContainerStyle={styles.contentContainer}
+    //   onScroll={scrollHandler}
+    //   scrollEventThrottle={16}
+    // >
+    //   {WORDS.map((title, index) => {
+    //     return <Page key={index.toString()} title={title} index={index} translateX={translateX} />;
+    //   })}
+    // </Animated.ScrollView>
+
+    // ----------------------------Interpolate Colors----------------------------
+
+    <Animated.View style={[styles.container, rStyles]}>
+      <Animated.Text style={[styles.text,rTextStyles]}>Theme</Animated.Text>
+      <Animated.View style={[styles.circle, rCircleStyles]}>
+        <Switch
+          value={theme === "dark"}
+          onValueChange={(toggled) => {
+            setTheme(toggled ? "dark" : "light");
+          }}
+          trackColor={SWITCH_TRACK_COLOR}
+          thumbColor={"violet"}
+        />
+      </Animated.View>
+    </Animated.View>
   );
 }
+
+const SIZE = Dimensions.get("window").width * 0.7;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   contentContainer: {
     flexGrow: 1,
-    alignItems:"flex-end"
+    alignItems: "flex-end",
   },
   button: {
     backgroundColor: "cyan",
@@ -166,12 +261,20 @@ const styles = StyleSheet.create({
     borderRadius: 20,
   },
   circle: {
-    width: CIRCLE_RADIUS * 2,
-    height: CIRCLE_RADIUS * 2,
+    width: SIZE,
+    height: SIZE,
+    backgroundColor: "#FFF",
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: CIRCLE_RADIUS,
-    borderWidth: 5,
-    borderColor: "rgba(0,0,256,0.5)",
+    borderRadius: SIZE / 2,
+    shadowColor: "black",
+    elevation: 18,
+  },
+  text: {
+    fontSize: 70,
+    textTransform: "uppercase",
+    fontWeight: "700",
+    letterSpacing: 14,
+    marginBottom: 35,
   },
 });
