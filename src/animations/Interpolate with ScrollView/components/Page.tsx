772d45f2d32b206ce4c97e02b7461ec8173c0527
tharuxpert <tharuxpert@gmail.com>
2025-04-08 12:59:58 +0530

    pinchGestureHandler

--- src/animations/Interpolate with ScrollView/components/Page.tsx
+++ src/animations/Interpolate with ScrollView/components/Page.tsx
@@ -1,78 +1,75 @@
 import React from "react";
-import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { Dimensions, View, StyleSheet, Text } from "react-native";
 import Animated, {
-  Extrapolation,
+  Extrapolate,
   interpolate,
   useAnimatedStyle,
 } from "react-native-reanimated";
-
-interface PageProps {
-  title: string;
-  index: number;
-  translateX: Animated.SharedValue<number>;
-}
 
 const { height, width } = Dimensions.get("window");
 
 const SIZE = width * 0.7;
 
-const Page: React.FC<PageProps> = ({ index, title, translateX }) => {
-  const inuptRange = [(index - 1) * width, index * width, (index + 1) * width];
+interface PageProps {
+  index: number;
+  translateX: Animated.SharedValue<number>;
+  title: string;
+}
+
+const Page: React.FC<PageProps> = ({ index, translateX, title }) => {
+  const inputRange = [(-index - 1) * width, index * width, (index + 1) * width];
 
   const rStyle = useAnimatedStyle(() => {
     const scale = interpolate(
       translateX.value,
-      inuptRange,
+      inputRange,
       [0, 1, 0],
-      Extrapolation.CLAMP
+      Extrapolate.CLAMP
     );
 
     const borderRadius = interpolate(
       translateX.value,
-      inuptRange,
+      inputRange,
       [0, SIZE / 2, 0],
-      Extrapolation.CLAMP
+      Extrapolate.CLAMP
     );
 
     return {
-      transform: [{ scale }],
       borderRadius,
+      transform: [{ scale }],
     };
   });
 
   const rTextStyle = useAnimatedStyle(() => {
     const translateY = interpolate(
       translateX.value,
-      inuptRange,
+      inputRange,
       [height / 2, 0, -height / 2],
-      Extrapolation.CLAMP
+      Extrapolate.CLAMP
     );
 
     const opacity = interpolate(
       translateX.value,
-      inuptRange,
+      inputRange,
       [-2, 1, -2],
-      Extrapolation.CLAMP
+      Extrapolate.CLAMP
     );
 
     return {
       opacity,
-      transform: [{ translateY }],
+      transform: [{ translateY: translateY }],
     };
   });
 
   return (
     <View
       style={[
-        styles.pageContainer,
-        {
-          backgroundColor: `rgba(0,0,256,0.${index + 2}
-    )`,
-        },
+        styles.container,
+        { backgroundColor: `rgba(0,0,256, 0.${index + 2})` },
       ]}
     >
       <Animated.View style={[styles.square, rStyle]} />
-      <Animated.View style={[{ position: "absolute" }, rTextStyle]}>
+      <Animated.View style={[styles.textContainer, rTextStyle]}>
         <Text style={styles.text}>{title}</Text>
       </Animated.View>
     </View>
@@ -80,23 +77,24 @@ const Page: React.FC<PageProps> = ({ index, title, translateX }) => {
 };
 
 const styles = StyleSheet.create({
-  pageContainer: {
-    height,
+  container: {
     width,
+    height,
     alignItems: "center",
     justifyContent: "center",
   },
   square: {
-    height: SIZE,
     width: SIZE,
-    backgroundColor: "rgba(0,0,256,0.4)",
+    height: SIZE,
+    backgroundColor: "rgba(0, 0, 256, 0.4)",
   },
   text: {
-    fontSize: 70,
+    fontSize: 60,
     color: "white",
     textTransform: "uppercase",
     fontWeight: "700",
   },
+  textContainer: { position: "absolute" },
 });
 
 export { Page };
